a2528ace2a63fe70e41e49a46e29a7ca09a701cd
James Fisher <fisher.james@outlook.com>
2020-12-04 20:54:05 -0800

    2020/5 setup

---
+++ 2020/5.py
@@ -0,0 +1,17 @@
+TESTS = '''
+'''
+
+INPUT = '''
+'''
+
+def solve(cases):
+    result = ''
+    return result
+
+TEST_RESULT = ''
+
+if __name__ == '__main__':
+    test_results = solve(INPUT)
+    print('results', test_results)
+    if test_results != TEST_RESULT:
+        exit()
