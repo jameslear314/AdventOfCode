de07bc3fcb8bc5a0d106863b963be2dbec55a43f
James Fisher <fisher.james@outlook.com>
2020-12-05 20:39:43 -0800

    2020/6 initial setup

---
+++ 2020/6.py
@@ -0,0 +1,14 @@
+import math
+TESTS = {}
+
+INPUT = '''
+'''
+
+def solve(cases
+    result = None
+    return result
+
+
+if __name__ == '__main__':
+    test_results = solve(INPUT)
+    print('results', test_results)
