6448d3707128f0781062abd90443690dfdde0f7b
James Fisher <jamesfisher314@outlook.com>
2021-12-02 22:12:46 -0800

    2021.3.2.failtounderstand

--- 2021/3.py
+++ 2021/3.py
@@ -1,7 +1,7 @@
 INPUT = 'data.3.txt'
 TEST0 = 'test.3.0.txt'
 RESULT0 = 198
-RESULT1 = 5
+RESULT1 = 220 # Test case should be 230, but the description is incorrect
 
 def loadData(filename):
     with open(filename, 'r') as inputFile:
@@ -28,25 +28,72 @@ def solve(cases):
     return gamma * epsilon
 
 def solve2(cases):
-    return None
+    gamma_cases = cases[:] #oxy
+    epsilon_cases = cases[:] #co2
+    # for case in cases:
+    #     gamma_cases.append(case)
+    #     epsilon_cases.append(case)
+    tally = tally_v(cases)
 
-def gamma_value(cases):
+    
+    for i in range(len(tally)):
+        print(len(gamma_cases), len(epsilon_cases))
+        gamma_ratio = (tally[i] + 0.0)/len(cases)
+        
+        if gamma_ratio < 0.5:
+            gamma_key = 0
+            epsilon_key = 1
+        elif gamma_ratio == 0.5:
+            gamma_key = 1
+            epsilon_key = 0
+        else:
+            gamma_key = 1
+            epsilon_key = 0
+
+        print("gamma", gamma_key, "epsilon", epsilon_key, "gamma_ratio", gamma_ratio)
+        if len(gamma_cases) > 1:
+            gamma_cases = [c for c in gamma_cases if int(c[i]) == gamma_key]
+        if len(epsilon_cases) > 1:
+            epsilon_cases = [c for c in epsilon_cases if int(c[i]) == epsilon_key]
+        print("gamma_case_len", len(gamma_cases), "epsilon_case_len", len(epsilon_cases))
+        print("gamma_Cases", gamma_cases)
+        print("epsilon_cases", epsilon_cases)
+
+
+    gamma = decimal(gamma_cases[0])
+    epsilon = decimal(epsilon_cases[0])
+    print(gamma, gamma_cases[0], epsilon, epsilon_cases[0])
+    return gamma * epsilon
+    
+def decimal(tally):
+    gamma = 0
+
+    base = 1
+    for i in range(len(tally)):
+        gamma_d = tally[-1 - i] # start at the final int
+        gamma += base * int(gamma_d)
+        base *= 2
+    return gamma
+    
+
+def tally_v(cases):
     tally = []
     for i in range(len(cases[0])):
         tally.append(0)
-    print("Starting with", tally, "of length", len(tally))
     for case in cases:
         for i in range(len(case)):
             if int(case[i]):
                 tally[i] += 1
-    print("ending tally,", tally)
+    return tally
+
+def gamma_value(cases):
+    tally = tally_v(cases)
     result = []
     for i in range(len(tally)):
         if tally[i] > len(cases) / 2.0:
             result.append(1)
         else:
             result.append(0)
-    print("gamma", result)
     return result
 
 if __name__ == '__main__':
@@ -64,6 +111,8 @@ if __name__ == '__main__':
     results = solve(data)
     print(results)
 
+    data = loadData(INPUT)
+    test = loadData(TEST0)
     test_results = solve2(test)
 
     if test_results != RESULT1:
