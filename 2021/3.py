5c039245c58afd407d45b9f011df35dcb2fb07a0
James Fisher <jamesfisher314@outlook.com>
2021-12-02 21:40:12 -0800

    2021.3.1

--- 2021/3.py
+++ 2021/3.py
@@ -10,16 +10,44 @@ def loadData(filename):
     return lines
 
 def solve(cases):
-    val = cases[0]
-    result = 0
-    for case in cases[1:]:
-        if case > val:
-            result += 1
-        val = case
-    return result
+    gamma_v = gamma_value(cases)
+    gamma = 0
+    epsilon = 0
+
+    base = 1
+    for i in range(len(gamma_v)):
+        gamma_d = gamma_v[-1 - i] # start at the final int
+        if gamma_d:
+            epsilon_d = 0
+        else:
+            epsilon_d = 1
+        gamma += base * gamma_d
+        epsilon += base * epsilon_d
+        base *= 2
+        
+    return gamma * epsilon
 
 def solve2(cases):
     return None
+
+def gamma_value(cases):
+    tally = []
+    for i in range(len(cases[0])):
+        tally.append(0)
+    print("Starting with", tally, "of length", len(tally))
+    for case in cases:
+        for i in range(len(case)):
+            if int(case[i]):
+                tally[i] += 1
+    print("ending tally,", tally)
+    result = []
+    for i in range(len(tally)):
+        if tally[i] > len(cases) / 2.0:
+            result.append(1)
+        else:
+            result.append(0)
+    print("gamma", result)
+    return result
 
 if __name__ == '__main__':
     data = loadData(INPUT)
