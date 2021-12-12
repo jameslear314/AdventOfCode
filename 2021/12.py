435a99a2c8d554d6943ec0ef750046c36e62b277
James Fisher <jamesfisher314@outlook.com>
2021-12-11 22:39:14 -0800

    Add more tests

--- 2021/12.py
+++ 2021/12.py
@@ -2,10 +2,13 @@ INPUT = 'data.12.txt'
 TEST0 = 'test.12.0.txt'
 TEST1 = 'test.12.1.txt'
 TEST2 = 'test.12.2.txt'
-RESULT0 = 10
+RESULT00 = 10
 RESULT01 = 19
 RESULT02 = 226
 RESULT1 = 5
+
+start = 'start'
+end = 'end'
 
 def loadData(filename):
     with open(filename, 'r') as inputFile:
@@ -19,17 +22,38 @@ def solve(cases):
 def solve2(cases):
     return None
 
+def prep(links):
+    graph = []
+    for link in links:
+        ends = link.split('-')
+        graph.append((ends[0], ends[1]))
+    
+    map = {}
+    for grap in graph:
+        for item in grap:
+            if item not in map:
+                map[item] = []
+        map[grap[0]] = grap[1]
+        map[grap[1]] = grap[0]
+    return map
+
 if __name__ == '__main__':
     data = loadData(INPUT)
     test = loadData(TEST0)
+    test1 = loadData(TEST1)
+    test2 = loadData(TEST2)
 
     print(test)
 
-    test_results = solve(test)
-    if test_results != RESULT0:
-        print(test_results, 'should be {}'.format(RESULT0))
-        exit()
-    print('results', test_results)
+    test = 0
+    for pair in [(test, RESULT00), (test1, RESULT01), (test2, RESULT02)]:
+        print("Test number", test, end=": ")
+        test_results = solve(pair[0])
+        if test_results != pair[1]:
+            print(test_results, 'should be {}'.format(pair[1]))
+            exit()
+        print('results', test_results)
+        test += 1
 
     results = solve(data)
     print(results)
