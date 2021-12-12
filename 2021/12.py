a3c2e903f1160db7903708d10ec7a4e01a3ce3b1
James Fisher <jamesfisher314@outlook.com>
2021-12-11 22:30:40 -0800

    2021.12.0

---
+++ 2021/12.py
@@ -0,0 +1,44 @@
+INPUT = 'data.12.txt'
+TEST0 = 'test.12.0.txt'
+TEST1 = 'test.12.1.txt'
+TEST2 = 'test.12.2.txt'
+RESULT0 = 10
+RESULT01 = 19
+RESULT02 = 226
+RESULT1 = 5
+
+def loadData(filename):
+    with open(filename, 'r') as inputFile:
+        lines = inputFile.readlines()
+    lines = [l.strip() for l in lines]
+    return lines
+
+def solve(cases):
+    return None
+
+def solve2(cases):
+    return None
+
+if __name__ == '__main__':
+    data = loadData(INPUT)
+    test = loadData(TEST0)
+
+    print(test)
+
+    test_results = solve(test)
+    if test_results != RESULT0:
+        print(test_results, 'should be {}'.format(RESULT0))
+        exit()
+    print('results', test_results)
+
+    results = solve(data)
+    print(results)
+
+    test_results = solve2(test)
+
+    if test_results != RESULT1:
+        print(test_results, 'should be {}'.format(RESULT1))
+        exit()
+
+    results = solve2(data)
+    print(results)
