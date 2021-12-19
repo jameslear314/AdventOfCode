17cf060657b067809b310ce67c588518a680a78f
James Fisher <jamesfisher314@outlook.com>
2021-12-18 22:55:44 -0800

    2021.15.1

--- 2021/15.py
+++ 2021/15.py
@@ -1,8 +1,10 @@
 INPUT = 'data.15.txt'
 TEST0 = 'test.15.0.txt'
 RESULT0 = 40
-RESULT1 = 5
+RESULT1 = 315
 import math
+
+MAXSCORE = 1000 * 1000 * 1000 * 1000 # /shrug # This puzzle probably doesn't reach a score of 1 Trillion
 
 def loadData(filename):
     with open(filename, 'r') as inputFile:
@@ -10,8 +12,90 @@ def loadData(filename):
     lines = [l.strip() for l in lines]
     return lines
 
+class Record:
+    score, x, y = None, None, None
+    xlow, xhigh = None, None
+    ylow, yhigh = None, None
+
+    def __init__(self, x, y, grid, last=None) -> None:
+        self.x, self.y = x, y
+        if last is not None:
+            self.score = last.score + grid[x][y]
+        else:
+            self.score = 0
+        if x != 0:
+            self.xlow = True
+        if y != 0:
+            self.ylow = True
+        if x < len(grid) - 1:
+            self.xhigh = True
+        if y < len(grid[x]) - 1:
+            self.yhigh = True
+
+    def __repr__(self) -> str:
+        return ''.join([str(self.x), ',', str(self.y), ': ', str(self.score)])
+
 def solve(cases):
-    grid = prep(cases)
+    grid, scores, visited = prep(cases)
+    endx, endy = len(grid), len(grid[0])
+    scoreReached = 0
+
+    records = {}
+    x = 0
+    y = 0
+    start = Record(x, y, grid)
+    scores[x][y] = start.score
+    records[(x, y)] = start
+    visited[x][y] = True
+    currents = {}
+    for item in Record(1, 0, grid, start), Record(0, 1, grid, start):
+        score = item.score
+        x, y = item.x, item.y
+        if x == endx and y == endy: # When it stumbles on the end, stop travelling
+            return item.score
+        if visited[x][y]:
+            continue
+        if score not in currents:
+            currents[score] = []
+        currents[score].append(item)
+
+    loop = 0
+    while len(currents) > 0: # If the item is the end, it bails
+        loop += 1
+        nexts = {}
+        keys = list(currents.keys())
+        keys.sort()
+        for key in keys:
+            points = currents[key]
+            items = set()
+            for point in points:
+                x, y = point.x, point.y
+                if x == endx - 1 and y == endy - 1:
+                    return point.score
+
+                visit = visited[x][y]
+                if visit:
+                    continue
+                visited[x][y] = True
+
+                # Consider the adjacent points
+                potentials = []
+                if point.xlow:
+                    potentials.append(Record(x - 1, y, grid, point))
+                if point.ylow:
+                    potentials.append(Record(x, y - 1, grid, point))
+                if point.xhigh: 
+                    potentials.append(Record(x + 1, y, grid, point))
+                if point.yhigh:
+                    potentials.append(Record(x, y + 1, grid, point))
+                for potential in potentials:
+                    if not visited[potential.x][potential.y]:
+                        score = potential.score
+                        if score not in nexts:
+                            nexts[score] = []
+                        nexts[score].append(potential)
+
+        currents = nexts
 
 
 def solve2(cases):
@@ -19,20 +103,24 @@ def solve2(cases):
 
 
 def prep(cases):
-    grid = []
-    for case in cases:
-        row = []
-        for char in case:
-            row.append(int(char))
-        grid.append(row)
-    return grid
+    grid, scores, visited = {}, {}, {}
+    for i in range(len(cases)):
+        row, scorerow, visitedrow = {}, {}, {}
+        for j in range(len(cases[i])):
+            row[j] = int(cases[i][j])
+            scorerow[j] = MAXSCORE
+            visitedrow[j] = False
+        grid[i] = row
+        scores[i] = scorerow
+        visited[i] = visitedrow
+
+    return grid, scores, visited
 
 if __name__ == '__main__':
     data = loadData(INPUT)
     test = loadData(TEST0)
 
     print(test)
-    grid = prep(test)
 
     test_results = solve(test)
     if test_results != RESULT0:
