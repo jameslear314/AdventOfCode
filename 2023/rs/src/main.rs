914d0c6611b54340ccbaf22fdef11f7822566545
James Lear <jameslear314@gmail.com>
2024-01-02 20:53:40 -0800

    Attempt 2023.01.b

--- 2023/rs/src/main.rs
+++ 2023/rs/src/main.rs
@@ -1,6 +1,15 @@
 mod first;
 
+#[macro_use]
+extern crate lazy_static;
+
 fn main() {
     println!("Hello, advent!");
+    day1();
+}
+
+fn day1() {
+    println!("~~~ day the first ~~~");
     first::a();
+    first::b();
 }
